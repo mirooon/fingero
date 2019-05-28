165040138998daeca39ea48fb0d5abff2e34c49e
devmood <devmood.business@gmail.com>
2019-05-28 21:48:07 +0200

    Added navbar

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import Canvas from './Canvas'
 import { CirclePicker } from 'react-color';
 import Save from './Save';
+import Navbar from './Navbar';
 
 class App extends Component {
   constructor(props) {
@@ -49,34 +50,46 @@ class App extends Component {
 
   setEraser = () => {
     console.log("Set eraser")
-    this.state.mode = "eraser";
+    this.setState({
+      mode: 'eraser'
+    });
   }
 
   setPen = () => {
     console.log("Set pen")
-    this.state.mode = "pen";
+    this.setState({
+      mode: 'pen'
+    });
   }
 
   setBrush = () => {
     console.log("Set brush")
-    this.state.mode = "brush";
+    this.setState({
+      mode: 'brush'
+    })
   }
 
   thicknessUp = () => {
     console.log("Current thickness: " + this.state.thickness)
-    this.state.thickness++;
+    this.setState({
+      thickness: this.state.thickness + 1
+    });
   }
 
   thicknessDown = () => {
     console.log("Current thickness: " + this.state.thickness)
     if (this.state.thickness > 0) {
-      this.state.thickness--;
+      this.setState({
+        thickness: this.state.thickness - 1
+      })
     }
   }
 
   changeColor = (color, event) => {
     console.log("Color changed" + color.hex);
-    this.state.color = color.hex;
+    this.setState({
+      color: color.hex
+    });
   }
 
   // download = () => {
@@ -96,56 +109,59 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-2">
-              <br /><br />
-              <h3>Tools</h3>
-              <br></br>
-              <Button variant="contained" color="primary" onClick={this.setEraser}>Eraser</Button>
-              <Button variant="contained" color="primary" onClick={this.setPen}>Pencil</Button>
-              <Button variant="contained" color="primary" onClick={this.setBrush}>Brush</Button>
-              {this.state.mode == "eraser" || this.state.mode == "brush"
-                ? <div>
-                  <br /> <br /> <br />
-                  <strong>Thickness</strong>
-                  <br /> <br />
-                  <Button variant="contained" color="primary" onClick={this.thicknessUp}>+</Button>
-                  <Button variant="contained" color="primary" onClick={this.thicknessDown}>-</Button>
-                </div> : null
-              }
-            </div>
-            <div class="col-md-8">
-              <br /><br />
-              <Button variant="contained" color="primary" onClick={this.startVideo}>Start video</Button>
-              <Button variant="contained" color="primary" onClick={this.stopVideo}>Stop video</Button>
-              <Save />
-              <br /> <br /> <br />
-              <Canvas
-                width={this.state.width}
-                height={this.state.height}
-                point={this.state.point}
-                mode={this.state.mode}
-                thickness={this.state.thickness}
-                color={this.state.color} />
-              <br />
-              <Webcam
-                ref={webcam => this.webcam = webcam}
-                screenshotFormat="image/png"
-                width={this.state.width}
-                height={this.state.height}
-              />
-            </div>
-            <div class="col-md-2">
-              <br /><br />
-              <h3>Colors</h3>
-              <br></br>
-              <CirclePicker onChange={this.changeColor} />
+      <React.Fragment>
+        <div className="App">
+          <Navbar />
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-2">
+                <br /><br />
+                <h3>Tools</h3>
+                <br></br>
+                <Button variant="contained" color="primary" onClick={this.setEraser}>Eraser</Button>
+                <Button variant="contained" color="primary" onClick={this.setPen}>Pencil</Button>
+                <Button variant="contained" color="primary" onClick={this.setBrush}>Brush</Button>
+                {this.state.mode == "eraser" || this.state.mode == "brush"
+                  ? <div>
+                    <br /> <br /> <br />
+                    <strong>Thickness</strong>
+                    <br /> <br />
+                    <Button variant="contained" color="primary" onClick={this.thicknessUp}>+</Button>
+                    <Button variant="contained" color="primary" onClick={this.thicknessDown}>-</Button>
+                  </div> : null
+                }
+              </div>
+              <div className="col-md-8">
+                <br /><br />
+                <Button variant="contained" color="primary" onClick={this.startVideo}>Start video</Button>
+                <Button variant="contained" color="primary" onClick={this.stopVideo}>Stop video</Button>
+                <Save />
+                <br /> <br /> <br />
+                <Canvas
+                  width={this.state.width}
+                  height={this.state.height}
+                  point={this.state.point}
+                  mode={this.state.mode}
+                  thickness={this.state.thickness}
+                  color={this.state.color} />
+                <br />
+                <Webcam
+                  ref={webcam => this.webcam = webcam}
+                  screenshotFormat="image/png"
+                  width={this.state.width}
+                  height={this.state.height}
+                />
+              </div>
+              <div className="col-md-2">
+                <br /><br />
+                <h3>Colors</h3>
+                <br></br>
+                <CirclePicker onChange={this.changeColor} />
+              </div>
             </div>
           </div>
-        </div>
-      </div >
+        </div >
+      </React.Fragment>
     );
   }
 }
